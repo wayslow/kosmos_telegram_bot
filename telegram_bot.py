575adb40e25517e5fd9c84002fdbbb3caabee4ba
wayslow <120993943+wayslow@users.noreply.github.com>
2023-01-26 19:34:14 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,15 +2,14 @@ import os
 from dotenv import load_dotenv
 
 import telegram
+
 load_dotenv()
 api_token = os.getenv('API_TOKEN_TELEGRAM')
-bot = telegram.Bot(token=api_token)
-
-
+BOT = telegram.Bot(token=api_token)
 
 def main():
     chat_id = "@photo_kosmos"
-    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    BOT.send_document(chat_id=chat_id, document=open(filename, 'rb'))
 
 if __name__ == '__main__':
     main()
