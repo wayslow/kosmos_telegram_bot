ba1896155274db08672eff487fbbc62d45f9ffc3
wayslow <120993943+wayslow@users.noreply.github.com>
2023-01-26 19:46:33 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,11 +1,11 @@
 import requests
 
 def download_photo(photo_url, filename, params=None):
-    print(params)
     response = requests.get(photo_url,params=params)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
+    print(f"{filename}")
 
 
 
