1720b7d8e4b2d6b961152d7109d6dc637ce025c9
wayslow <120993943+wayslow@users.noreply.github.com>
2023-01-30 19:43:31 +0300

    Update nasa_epik_photo.py

--- nasa_epik_photo.py
+++ nasa_epik_photo.py
@@ -7,7 +7,7 @@ import requests
 
 from download import download_photo
 
-def get_nasa_epik(url, api_token):
+def get_nasa_epic(url, api_token):
 
     params = {
         "api_key": api_token,
@@ -19,19 +19,20 @@ def get_nasa_epik(url, api_token):
     return response.json(), params
 
 
-def formatting_download_url(url_nasa_epik_photos, folder_name, api_token):
-    photo_info, params=get_nasa_epik(url_nasa_epik_photos, api_token)
+def get_nasa_epic_photos(url_nasa_epic_photos, folder_name, api_token, type_file="png"):
+
+    photo_info, params=get_nasa_epic(url_nasa_epic_photos, api_token)
 
     for index, images in enumerate(photo_info):
         date_time_obj = datetime.datetime.fromisoformat(images["date"])
         date =datetime.date(date_time_obj.year, date_time_obj.month, date_time_obj.day)
         formatted_date = date.strftime("%Y/%m/%d")
         name_image = data["image"]
-        type_file ="png"
-        photo_url = f"https://epic.gsfc.nasa.gov/archive/natural/{formatted_date}/{type_file}/{name_image}.{type_file}"
-        path = os.path.join(folder_name, f"nasa_epik{index}.{type_file}")
-        download_photo(photo_url, path, params)
+        file_name = f"{name_image}.{type_file}"
+        photo_url = f"https://epic.gsfc.nasa.gov/archive/natural/{formatted_date}/{type_file}/{file_name}"
+        path = os.path.join(folder_name, file_name)
 
+        download_photo(photo_url, path, params)
 
 
 def main():
@@ -39,11 +40,11 @@ def main():
     pathlib.Path(folder_name).mkdir(parents=True, exist_ok=True)
 
     load_dotenv()
-    api_token = os.getenv('API_TOKEN_NASA')
+    nasa_api_token = os.getenv('API_TOKEN_NASA')
 
-    url_nasa_epik_photos = '  https://api.nasa.gov/EPIC/api/natural/images'
+    url_nasa_epic_photos = 'https://api.nasa.gov/EPIC/api/natural/images'
 
-    formatting_download_url(url_nasa_epik_photos, folder_name, api_token)
+    get_nasa_epic_photos(url_nasa_epic_photos, folder_name, nasa_api_token)
 
 
 if __name__ == '__main__':
