c1176ae9e8c5f70d6b9bc4f52ff90b6304779082
wayslow <120993943+wayslow@users.noreply.github.com>
2023-01-30 19:43:33 +0300

    Update nasa_apod_photos.py

--- nasa_apod_photos.py
+++ nasa_apod_photos.py
@@ -7,23 +7,22 @@ from dotenv import load_dotenv
 
 from download import download_photo
 
-def get_nasa_apod_photos(url, folder_name, api_token):
+def get_nasa_apod_photos(nasa_apod_url, folder_name, api_token):
     params= {
         "api_key": api_token,
         "count": 40
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get(nasa_apod_url, params=params)
     response.raise_for_status()
 
-    for index, data in enumerate(response.json()):
-        photo_url = data['url']
-        parse = urllib.parse.urlsplit(photo_url)
-        extension = os.path.splitext(parse.path)[1]
+    for index, photo_apod in enumerate(response.json()):
+        photo_apod_url = photo_apod['url']
+        parse = urllib.parse.urlsplit(photo_apod_url)
+        extension = os.path.splitext(parse.path)[-1]
         path = os.path.join(folder_name, f"nasa_apod{index}{extension}")
 
-        download_photo(photo_url,path,params)
-
+        download_photo(photo_url, path, params)
 
 
 def main():
@@ -33,9 +32,9 @@ def main():
     load_dotenv()
     api_token = os.getenv('API_TOKEN_NASA')
 
-    url_nasa_apod_photos = "https://api.nasa.gov/planetary/apod"
+    nasa_apod_url = "https://api.nasa.gov/planetary/apod"
 
-    get_nasa_apod_photos(url_nasa_apod_photos, folder_name, api_token)
+    get_nasa_apod_photos(nasa_apod_url, folder_name, api_token)
 
 
 if __name__ == '__main__':
