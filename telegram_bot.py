65fd2f2aa4c04a37e059b58bfe10fb4277354676
wayslow <120993943+wayslow@users.noreply.github.com>
2023-02-02 18:17:16 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 import telegram
 
 load_dotenv()
-api_token = os.getenv('API_TOKEN_TELEGRAM')
+api_token = os.getenv('TELEGRAM_API_TOKEN')
 BOT = telegram.Bot(token=api_token)
 
 def main():
     chat_id = "@photo_kosmos"
-    BOT.send_document(chat_id=chat_id, document=open(filename, 'rb'))
+    BOT.send_photo(chat_id=chat_id, photo=open('image/epic_1b_20230126000830.png', 'rb'))
+
 
 if __name__ == '__main__':
     main()
